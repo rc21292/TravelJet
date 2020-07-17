8e2bc99f4ca41d650a11369f453fdf4f65caf6f5
Prashant Singh <websales9999@gmail.com>
2020-07-17 14:18:48 -0700

    first commit by Prashant

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,66 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+    @include('layouts.front-head')
+</head>
+<body>
+ <!-- Sign In -->
+ <div class="signIn">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <h1>Hey, Welcome Back</h1>
+                <h3>Sign in using social media accounts</h3>
+                <ul class="list-inline social-icons">
+                    <li><a href="#"><i class="fa fa-facebook fb-icon"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter tw-icon"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google go-icon"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram insta-icon"></i></a></li>
+                </ul>
+                <span>Or use your email account:</span>
+                <div class="signIn_form">
+                 <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="form-group required">
+                        <label>Email</label>
+                        <input id="email" type="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="example@gmail.com">
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="form-group required">
+                        <label>Password</label>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Min 6 Characters Required">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <p class="forgot"><a href="{{ route('password.request') }}">Forgot your password ?</a></p>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-warning">Sign In</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="formBanner">
+                <div class="bannerText">
+                    <h2>Hello There, Join Us</h2>
+                    <p>Enter your personal detail and join the traveljet community for better experience</p>
+                    <a href="#" class="btn btn-primary">Sign Up</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+</div>
+<!-- Sign In // End-->
+</body>
+</html>
---
+++ resources/views/layouts/front-head.blade.php
@@ -0,0 +1,16 @@
+<title>Travel Jet</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="{{asset('frontend/css/bootstrap.min.css')}}" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,900|Poppins:300,400,500,600,700,900|Roboto:300,400,500,700,900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;500;600;700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+	<script src="{{asset('frontend/js/jquery-2.1.1.min.js')}}"></script>
+	<script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/animate.cs')}}s">
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/font-awesome/css/font-awesome.css')}}">
+	<script src="{{asset('frontend/js/selectize.min.js')}}" integrity="sha256-+C0A5Ilqmu4QcSPxrlGpaZxJ04VjsRjKu+G82kl5UJk=" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="{{asset('frontend/css/selectize.bootstrap3.min.css')}}" integrity="sha256-ze/OEYGcFbPRmvCnrSeKbRTtjG4vGLHXgOqsyLFTRjg=" crossorigin="anonymous" />
+	<link rel="stylesheet" type="text/css" href="{{asset('frontend/css/stylesheet.css')}}">
+	<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"> -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
