7d4c71b89150b8ba581258ce8bb35b3ed4c69b1f
krishna mishra <er.krishna.mishra@gmail.com>
2020-09-10 20:46:22 +0530

    Agent upcomming bookings

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -419,7 +419,7 @@ class QuotationController extends Controller
     public function getQuotationPayment($id,$user_id)
     {
         // $quotation = Quotation::where('user_id',$user_id)->where('booking_id',$id)->where('status','awarded')->first();
-         $quotation = Quotation::where('user_id',$user_id)->where('booking_id',$id)->where('status','awarded')->orWhere('status','pending')->first();
+         $quotation = Quotation::where('user_id',$user_id)->where('booking_id',$id)->where('status','pending')->first();
         // echo "<pre>";print_r($quotation);"</pre>";exit;
         if (!empty($quotation->payments)) {
 
