01d7dfffe619f3837d4dd3c9709c5d018a8fd701
krishna mishra <er.krishna.mishra@gmail.com>
2020-10-08 10:16:33 +0530

    notices

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -46,7 +46,7 @@ class NoticeController extends Controller
         $user_data = User::where('id',$user_id)->select('role')->first();
         $user_created_at = User::where('id',$user_id)->value('created_at');
         if ($user_data->role === 'agent') {
-            return $notices = $this->notice::whereIn('type', ['job_post','award'])->where('created_at','>=',$user_created_at)->where('receiver_id',$user_id)->orderBy('created_at','DESC')->get();
+            return $notices = $this->notice::whereIn('type', ['job_post','award'])->where('created_at','>=',$user_created_at)->whereIn('receiver_id',[0,$user_id])->orderBy('created_at','DESC')->get();
         } elseif ($user_data->role === 'admin') {
             $notices = $this->notice::where('receiver_id',1)->orderBy('created_at','DESC');
             return $notices->get();
@@ -60,7 +60,7 @@ class NoticeController extends Controller
         $user_id = $id;
         $user_data = User::where('id',$user_id)->select('role')->first();
         $user_created_at = User::where('id',$user_id)->value('created_at');
-        $notices = $this->notice::whereIn('type', ['job_post','award'])->where('created_at','>=',$user_created_at)->where('receiver_id',$user_id)->orderBy('created_at','DESC');
+        $notices = $this->notice::whereIn('type', ['job_post','award'])->where('created_at','>=',$user_created_at)->whereIn('receiver_id',[0,$user_id])->orderBy('created_at','DESC');
         return $notices->take(5)->get();
         
     }
